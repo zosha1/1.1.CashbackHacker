762cb57bc09409e0dbd5fce25fa3d2736fcef512
Zosha <posyamyka@gmail.com>
2021-07-30 01:00:22 +0300

    TestNG-2

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -10,20 +10,20 @@ public class CashbackHackServiceTest {
     public void testRemain700() {
         CashbackHackService cashbackHackService = new CashbackHackService();
 
-        assertEquals(cashbackHackService.remain(700), 300);
+        assertEquals(300, cashbackHackService.remain(700));
     }
 
     @Test
     public void testRemain1000() {
         CashbackHackService cashbackHackService = new CashbackHackService();
 
-        assertEquals(cashbackHackService.remain(1000), 0);
+        assertEquals(0, cashbackHackService.remain(1000));
     }
 
     @Test
     public void testRemain4300() {
         CashbackHackService cashbackHackService = new CashbackHackService();
 
-        assertEquals(cashbackHackService.remain(4300), 700);
+        assertEquals(700, cashbackHackService.remain(4300));
     }
 }
