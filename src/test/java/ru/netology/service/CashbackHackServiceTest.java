e08f523f1f6f74bb5d9cc31900cab5b9c0a2f191
Zosha <posyamyka@gmail.com>
2021-07-30 01:41:10 +0300

    JUnit5 и Legacy

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,10 +1,25 @@
 package ru.netology.service;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.Assert.*;
 
 public class CashbackHackServiceTest {
+
+    @Test
+    public void testRemain750() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+
+        Assertions.assertEquals(250, cashbackHackService.remain(750));
+    }
+
+    @Test
+    public void testRemain0() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+
+        Assertions.assertEquals(1000, cashbackHackService.remain(0));
+    }
 
     @Test
     public void testRemain700() {
